938b18648762c766650198c05ac39e315fd5901d
John Gbenga Ayodele <116895781+Techie-John@users.noreply.github.com>
2025-08-17 23:47:02 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,3 @@
--- Task 4: Show full description of books table without using DESCRIBE, EXPLAIN or ANALYZE
+-- Task 4: Show full description of books table
 USE alx_book_store;
-SHOW COLUMNS FROM Books;
+SHOW COLUMNS FROM books;
