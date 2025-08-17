8b59f0d3981f49cf7f487736d5c62bbf9ca87b44
John Gbenga Ayodele <116895781+Techie-John@users.noreply.github.com>
2025-08-17 23:47:58 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,5 @@
 -- Task 4: Show full description of books table
 USE alx_book_store;
-SHOW COLUMNS FROM books;
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
